8d5d755bda554fceb619946a505e3c811ba0c235
Manajot17 <mananjotsingh17@gmail.com>
2024-05-26 15:12:40 +0530

    create signin api route and controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js'
 import bcryptjs from 'bcryptjs'
 import errorHandler from '../utils/error.js';
+import jwt from "jsonwebtoken"
 
 
 const signup = async (req, res, next)=>{
@@ -31,4 +32,41 @@ const signup = async (req, res, next)=>{
 }
 
 
-export default signup;
+
+const signin = async (req, res, next) =>{
+    
+    const {email, password} = req.body;
+
+    if(!email || !password || email === '' || password === ''){
+        return next(errorHandler(400, "All fields are required"))
+    }
+
+    try {
+        const user = await User.findOne({email})
+        if(!user){
+            return next(errorHandler(404, "Invalid email or password"))
+        }
+
+        const isCorrectPass = bcryptjs.compareSync(password, user.password);
+        if(!isCorrectPass){
+            return next(errorHandler(404, "Invalid email or password"))
+        }
+
+        const token = jwt.sign(
+            {id: user._id}, process.env.JWT_SECRET, 
+        )
+
+        //removing password info 
+        const {password: pass, ...rest} = user._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+export {signup, signin};
+
